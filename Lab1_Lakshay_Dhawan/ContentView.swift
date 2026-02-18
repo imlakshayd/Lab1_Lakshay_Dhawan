75c0e1b54a90e6c1a2db658b704452532c3b78c4
Lakshay Dhawan <lakshayd2002@gmail.com>
2026-02-18 17:25:04 -0500

    Initial Commit

---
+++ Lab1_Lakshay_Dhawan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Lakshay_Dhawan
+//
+//  Created by Lakshay Dhawan on 2026-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Lakshay_Dhawan/Lab1_Lakshay_DhawanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Lakshay_DhawanApp.swift
+//  Lab1_Lakshay_Dhawan
+//
+//  Created by Lakshay Dhawan on 2026-02-18.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Lakshay_DhawanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
