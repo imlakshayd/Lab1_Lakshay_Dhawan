c18c7e1d10aab1866932de31a75504490857d4f5
Lakshay Dhawan <lakshayd2002@gmail.com>
2026-03-09 15:59:57 -0400

    Add prime checking algorithm

--- GameViewModel.swift
+++ GameViewModel.swift
@@ -15,4 +15,17 @@ class GameViewModel: ObservableObject {
     func generateRandomNumber() {
         currentNumber = Int.random(in: 1...100)
     }
+    
+    func isNumberPrime(_ number: Int) -> Bool {
+        if number <= 1 { return false }
+        if number <= 3 { return true }
+        var i = 2
+        while i * i <= number {
+            if number % i == 0 {
+                return false
+            }
+            i += 1
+        }
+        return true
+    }
 }
